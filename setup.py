f0613f3baf5f06b5301ac0abe4fa3a79c611b00d
Juan Manuel Pérez <jmperez.85@gmail.com>
2016-10-06 16:01:29 -0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""distutils setup file."""
+from distutils.core import setup
+
+setup(
+    name='ply-examples',
+    version='1.0',
+    description='Examples of the Ply Library',
+    author='Juan Manuel Pérez',
+    author_email='jmperez.85@gmail.com',
+    packages=['examples'],
+)

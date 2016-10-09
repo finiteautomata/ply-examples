97a22deba1d38211891633c947689f67d5c4dfe9
Juan Manuel Perez <jmperez@dc.uba.ar>
2016-10-09 19:13:22 -0300

    Agrego test.

---
+++ examples/calculator_with_precedence/test.py
@@ -0,0 +1,24 @@
+"""Tests para parser."""
+import unittest
+from parser import parse
+
+
+class ParserTest(unittest.TestCase):
+    """Tests para el parser."""
+
+    def test_suma_bien_dos_numeros(self):
+        self.assertEqual(parse("1+1"), 2)
+
+    def test_suma_bien_tres_numeros(self):
+        self.assertEqual(parse("1+   2 + 3"), 6)
+
+    def test_multiplica_dos_numeros(self):
+        self.assertEqual(parse("3 * 4"), 12)
+
+    def test_multiplica_y_suma(self):
+        self.assertEqual(parse("2 * 4 + 3"), 11)
+
+    def test_suma_y_multiplica(self):
+        self.assertEqual(parse("2 + 4 * 3"), 14)
+if __name__ == '__main__':
+    unittest.main()
